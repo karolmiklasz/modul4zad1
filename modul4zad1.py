1a5d363d52909d144db8e176e6c56814ff0a65b0
Karol Miklasz <karol3wpl@gmail.com>
2023-10-08 17:08:34 +0200

    Define function

---
+++ modul4zad1.py
@@ -0,0 +1,14 @@
+def palindrom(text):
+    text = text.lower()
+
+    left = 0
+    right = len(text) - 1
+
+    while left < right:
+        if text[left] != text[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
